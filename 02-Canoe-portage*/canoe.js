3ac77fcddbb0562cee6a73a2c3f4d80e793454d2
amilalizada <elizade_amil@inbox.ru>
2020-04-20 16:23:29 +0400

    task_2

---
+++ 02-Canoe-portage*/canoe.js
@@ -0,0 +1,7 @@
+ var input = prompt('nece rod gedeceksiz?');
+ input = parseInt(input);
+ var m = input * 5.0292 * 3.1;
+ var f = input * 40 * 3.1;
+ var mil = input * 1609 * 3.1;
+ var foot = input * 0.3048 * 3.1;
+ alert('metr ile : ' + m + '\n' + ' furlong ile :' + f + '\n' + ' mil ile: ' + mil + '\n' + 'foot ile :' + foot);
