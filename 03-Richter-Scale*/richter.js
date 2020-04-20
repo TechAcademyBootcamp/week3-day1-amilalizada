931deedfa70bca67911e801344ac8cc93e2ea034
amilalizada <elizade_amil@inbox.ru>
2020-04-20 16:42:15 +0400

    task_3

---
+++ 03-Richter-Scale*/richter.js
@@ -0,0 +1,5 @@
+var input = prompt('rixter balini daxil edin:');
+iput = parseInt(input)
+var coil = 10 * (1.5 * input + 4.8);
+var tnt = 4.184 * 10 * 6 * coil;
+alert('coil=' + coil + '\n' + 'TNT=' + tnt);
