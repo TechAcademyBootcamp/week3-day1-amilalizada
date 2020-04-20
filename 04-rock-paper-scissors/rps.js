3dd42765bc6805c507d170abe5a0fc8bd8e280d4
amilalizada <elizade_amil@inbox.ru>
2020-04-20 17:55:15 +0400

    task_4

--- 04-rock-paper-scissors/rps.js
+++ 04-rock-paper-scissors/rps.js
@@ -1,16 +1,22 @@
 prompt('adinizi daxil edin');
-var k = 1;
-var d = 2;
-var q = 3;
+var paper = 1;
+var rock = 2;
+var scissors = 3;
 
-var input = prompt('bunlardan birini secin: ' + k + '-kagiz ' + d + '-das ' + q + '-qayci');
+var input = prompt('bunlardan birini secin: ' + paper + '-kagiz ' + rock + '-das ' + scissors + '-qayci');
+
 var random = Math.floor(Math.random() * 3 + 1);
-document.write('sizin sectiyiniz:' + input + '<br>');
-document.write('reqibinizin secdiyi:' + random + '<br>')
-if (input == 1 && random == 1 || input == 2 && random == 2 || input == 3 && random == 3) {
-    document.write('beraber');
-} else if (input == 1 && random == 2 || input == 2 && random == 3 || input == 3 && random == 1) {
-    document.write('siz qazandiniz!')
+if (input == paper || input == rock || input == scissors) {
+    document.write('sizin sectiyiniz:' + input + '<br>');
+    document.write('reqibinizin secdiyi:' + random + '<br>')
+    if (input == random) {
+        document.write('beraber');
+    } else if (input == paper && random == rock || input == rock && random == scissors || input == scissors && random == paper) {
+        document.write('siz qazandiniz!')
+    } else {
+        document.write('meglub oldunuz.')
+    }
 } else {
-    document.write('meglub oldunuz.')
+
+    document.write('seciminiz [1-3] araligindan kenardadir');
 }
